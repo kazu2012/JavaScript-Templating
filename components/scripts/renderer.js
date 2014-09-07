b5209f2e76a8b66e2fa2f9292af1855da8bc664a
Eric Njanga <njanga.webdev@gmail.com>
2014-09-07 09:59:27 -0400

    Adding jQuery formatters

--- components/scripts/renderer.js
+++ components/scripts/renderer.js
@@ -10,36 +10,65 @@ module.exports =(function(){
 
 
 	export_.renderTemplates = function (e) {
+           // define the formatter
+           $.addTemplateFormatter("UpperCaseFormatter", function(value, options) {
+               return value.toUpperCase();
+            });
 
-		console.log('----------- export_.renderTemplates = function (e) { -----------' );
+            // load the template from the external file
+            $("#container").loadTemplate("templateFormat.html", [{
+               "name" : "Henry Handsome",
+               "phone" : "+1-212-555-1234",
+               "email" : "pensive@example.com",
+               "title" : "Senior VP of Basketweaving",
+               "fulltime" : true
+            }, {
+               "name" : "Penelope Persistent",
+               "phone" : "+1-212-555-8000",
+               "email" : "truthful@example.com",
+               "title" : "Principal Understudy",
+               "fulltime" : false
+            }, {
+               "name" : "Sam Serendipity",
+               "phone" : "+1-212-555-9876",
+               "email" : "helpful@example.com",
+               "title" : "Chief Cook and Bottle Washer",
+               "fulltime" : true
+            }, {
+               "name" : "Tom Terriffic",
+               "phone" : "+1-212-555-0011",
+               "email" : "grumpy@example.com",
+               "title" : "Janitor",
+               "fulltime" : false
+            }]);
 
 
-
-        $("#container").loadTemplate( "template.html" , [{
-           "name" : "Henry Handsome",
-           "phone" : "+1-212-555-1234",
-           "email" : "pensive@example.com",
-           "title" : "Senior VP of Basketweaving",
-           "fulltime" : true
-        }, {
-           "name" : "Penelope Persistent",
-           "phone" : "+1-212-555-8000",
-           "email" : "truthful@example.com",
-           "title" : "Principal Understudy",
-           "fulltime" : false
-        }, {
-           "name" : "Sam Serendipity",
-           "phone" : "+1-212-555-9876",
-           "email" : "helpful@example.com",
-           "title" : "Chief Cook and Bottle Washer",
-           "fulltime" : true
-        }, {
-           "name" : "Tom Terriffic",
-           "phone" : "+1-212-555-0011",
-           "email" : "grumpy@example.com",
-           "title" : "Janitor",
-           "fulltime" : false
-        }], { isFile : true } );    
+		// //Loading an external file
+  //       $("#container").loadTemplate( "template.html" , [{
+  //          "name" : "Henry Handsome",
+  //          "phone" : "+1-212-555-1234",
+  //          "email" : "pensive@example.com",
+  //          "title" : "Senior VP of Basketweaving",
+  //          "fulltime" : true
+  //       }, {
+  //          "name" : "Penelope Persistent",
+  //          "phone" : "+1-212-555-8000",
+  //          "email" : "truthful@example.com",
+  //          "title" : "Principal Understudy",
+  //          "fulltime" : false
+  //       }, {
+  //          "name" : "Sam Serendipity",
+  //          "phone" : "+1-212-555-9876",
+  //          "email" : "helpful@example.com",
+  //          "title" : "Chief Cook and Bottle Washer",
+  //          "fulltime" : true
+  //       }, {
+  //          "name" : "Tom Terriffic",
+  //          "phone" : "+1-212-555-0011",
+  //          "email" : "grumpy@example.com",
+  //          "title" : "Janitor",
+  //          "fulltime" : false
+  //       }], { isFile : true } );    
 
 		console.log( $("template.html").length );
 
